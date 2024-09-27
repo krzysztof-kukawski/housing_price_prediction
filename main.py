034593858de1039695c970a79da8b9656bf2c50a
krzysztof-kukawski <krzysztof.kukawski.ln@gmail.com>
2024-09-27 20:52:20 +0200

    variable selection

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 import os
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 from statsmodels.api import OLS, add_constant
-from scipy import stats
+from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
 
 cwd = os.path.dirname(__file__)
 path_to_data = os.path.join(cwd, "data", "housing_processed.csv")
 
 housing = pd.read_csv(path_to_data)
 housing.dropna(inplace=True)
-housing.info()
-housing.shape
 housing.drop(columns=['ocean_proximity', 'Unnamed: 0', 'NEAR BAY'],axis=0,  inplace= True)
 features = housing.drop(['median_house_value'], axis=1)
 target = housing['median_house_value']
-
+correlation = features.corrwith(target)
+correlation = correlation.apply(lambda x: abs(x))
+most_correlated = correlation.sort_values(ascending=False).index[0:11]
 
 feature_train, feature_test, target_train, target_test = train_test_split(
-    features, target, test_size=0.33)
+    features[most_correlated], target, test_size=0.33)
 
 
 
 feature_train = add_constant(feature_train)
-model2 = OLS(endog=target_train, exog=feature_train).fit()
-res_summary = model2.summary()
+model = OLS(endog=target_train, exog=feature_train).fit()
+res_summary = model.summary()
+pred = model.predict(add_constant(feature_test))
 print(res_summary)
+print(mean_squared_error(target_test, pred))
+print(mean_absolute_percentage_error(target_test, pred))
 
