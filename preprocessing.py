998419cc9288d9e09ad441a2c5ad3cfda7f933e9
krzysztof-kukawski <krzysztof.kukawski.ln@gmail.com>
2024-09-28 00:08:55 +0200

    pipeline file for further development

--- preprocessing.py
+++ preprocessing.py
@@ -9,13 +9,13 @@ housing = pd.read_csv(path_to_data)
 
 enc = pd.get_dummies(housing['ocean_proximity'], drop_first= True, dtype='float')
 
-housing_enc = housing.merge(enc, 'inner', left_index=True, right_index=True)
-housing_enc.drop('ocean_proximity',axis=1,  inplace= True)
+housing_encoded = housing.merge(enc, 'inner', left_index=True, right_index=True)
+housing_encoded.drop('ocean_proximity',axis=1,  inplace= True)
 scaler = StandardScaler()
 scaler.set_output(transform='pandas')
-housing_transformed = scaler.fit_transform(housing_enc)
+housing_transformed = scaler.fit_transform(housing_encoded)
 print(housing_transformed)
 print(scaler.get_feature_names_out())
 
-housing_enc.to_csv(os.path.join(cwd, "data", "housing_processed.csv"))
+housing_encoded.to_csv(os.path.join(cwd, "data", "housing_processed.csv"))
 
